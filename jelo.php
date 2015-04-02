2313fa78561a20030d5b4e32baebb7a5dde60968
AntonG1 <tonii917@gmail.com>
2015-04-02 23:22:12 +0200

    Revert "DWA - seminar 1"
    This reverts commit a611e324d8dcab0ba55c0de8e7e6ea7a6e4c8643.

--- jelo.php
+++ jelo.php
@@ -1,5 +1,8 @@
 <!doctype html>
- <html class="no-js" lang="">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -14,7 +17,9 @@
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
-
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
 
         <div class="header-container">
 		
@@ -40,8 +45,8 @@
                 </article>
 
 
-            </div> 
-        </div> 
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
 
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
